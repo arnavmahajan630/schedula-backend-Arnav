ebc917d8fc21836388786e18530d278f18ca0d9f
Abhishek Devanda <abdevanda2424@gmail.com>
2025-06-17 13:20:19 +0530

    feat/added typeorm & pg and fixed lock files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HelloWorldModule],
+  imports: [
+    HelloWorldModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      // synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
