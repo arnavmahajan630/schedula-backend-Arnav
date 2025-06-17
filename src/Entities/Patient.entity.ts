8ab81ec51f9a32ae4dfd515ab433927c5cdbb15d
Tanmay Singh <131035696+TanmaySingh007@users.noreply.github.com>
2025-06-17 20:56:28 +0530

    Update Patient.entity.ts

--- src/Entities/Patient.entity.ts
+++ src/Entities/Patient.entity.ts
@@ -1,7 +1,34 @@
-interface Patient {
-  id: UUID;
-  userId: UUID;
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, OneToMany } from 'typeorm';
+import { User } from './User';
+import { Appointment } from './Appointment';
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+}
+
+@Entity()
+export class Patient {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @OneToOne(() => User, (user) => user.patientProfile)
+  @JoinColumn()
+  user: User;
+
+  @Column()
   age: number;
+
+  @Column('text', { array: true })
   medicalHistory: string[];
+
+  @Column({ type: 'enum', enum: Gender })
   gender: Gender;
+
+  @OneToMany(() => Appointment, (appointment) => appointment.patient)
+  appointments: Appointment[];
 }
