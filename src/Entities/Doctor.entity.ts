5ec781bbe1a556044a657f3e9d84e5db79a8de50
Tanmay Singh <131035696+TanmaySingh007@users.noreply.github.com>
2025-06-17 20:56:05 +0530

    Update Doctor.entity.ts

--- src/Entities/Doctor.entity.ts
+++ src/Entities/Doctor.entity.ts
@@ -1,7 +1,32 @@
-interface Doctor {
-  id: UUID;
-  userId: UUID;
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, OneToMany } from 'typeorm';
+import { User } from './User';
+import { TimeSlot } from './TimeSlot';
+import { Appointment } from './Appointment';
+
+@Entity()
+export class Doctor {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @OneToOne(() => User, (user) => user.doctorProfile)
+  @JoinColumn()
+  user: User;
+
+  @Column()
   specialization: string;
+
+  @Column()
   experience: number;
+
+  @Column('text', { array: true })
   qualifications: string[];
+
+  @OneToMany(() => TimeSlot, (slot) => slot.doctor)
+  timeSlots: TimeSlot[];
+
+  @OneToMany(() => Appointment, (appointment) => appointment.doctor)
+  appointments: Appointment[];
 }
