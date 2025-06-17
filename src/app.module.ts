cb79477ddc238c34e98ba1c96569a06a865ffb56
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-06-17 15:11:38 +0530

    Completed Creating Entities in TypeOrm. Tested and verified working Successfully on localhost

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloWorldModule } from './hello-world/hello-world.module';
+import { HelloWorldModule } from './Hello-World/hello-world.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Doctor } from './Entities/doctor.entity';
+import { Appointment } from './Entities/appointment.entity';
+import { Patient } from './Entities/patient.entity';
+import { TimeSlot } from './Entities/timeslot.entity';
 
 @Module({
   imports: [
     HelloWorldModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
-      entities: [],
-      // synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',  // Your real password
+      database: '', // the dbname
+      entities: [Doctor, Patient, TimeSlot, Appointment],
+      synchronize: true,
+
     }),
   ],
   controllers: [AppController],
