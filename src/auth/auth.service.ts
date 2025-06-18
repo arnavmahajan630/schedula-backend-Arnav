36a25502a530e5c0179e301ed34a200470e0d045
Abhishek Devanda <abdevanda2424@gmail.com>
2025-06-18 23:21:30 +0530

    fixed/removed try catch in auth functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import {
   ConflictException,
   Injectable,
-  ServiceUnavailableException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -20,73 +19,61 @@ export class AuthService {
   ) {}
 
   async signup(doctorSignupDto: DoctorSignupDto) {
-    try {
-      const alredyExists = await this.doctorRepository.findOne({
-        where: { email: doctorSignupDto.email },
-      });
-      if (alredyExists) {
-        throw new ConflictException(
-          'Email already in use. Please use a different email.',
-        );
-      }
-      const hashed_password = await this.hashString(doctorSignupDto.password);
-
-      const newDoctor = this.doctorRepository.create({
-        first_name: doctorSignupDto.first_name,
-        last_name: doctorSignupDto.last_name,
-        email: doctorSignupDto.email,
-        password_hash: hashed_password,
-        phone_number: doctorSignupDto.phone_number,
-        education: doctorSignupDto.education,
-        experience_years: doctorSignupDto.experience_years,
-        specialization: doctorSignupDto.specialization,
-        clinic_name: doctorSignupDto.clinic_name,
-        clinic_address: doctorSignupDto.clinic_address,
-        available_days: doctorSignupDto.available_days,
-        available_time_slots: doctorSignupDto.available_time_slots,
-      });
-
-      const savedDoctor = await this.doctorRepository.save(newDoctor);
-      savedDoctor.password_hash = '';
-
-      return savedDoctor;
-    } catch {
-      throw new ServiceUnavailableException(
-        'Service is currently unavailable. Please try again later.',
+    const alredyExists = await this.doctorRepository.findOne({
+      where: { email: doctorSignupDto.email },
+    });
+    if (alredyExists) {
+      throw new ConflictException(
+        'Email already in use. Please use a different email.',
       );
     }
+    const hashed_password = await this.hashString(doctorSignupDto.password);
+
+    const newDoctor = this.doctorRepository.create({
+      first_name: doctorSignupDto.first_name,
+      last_name: doctorSignupDto.last_name,
+      email: doctorSignupDto.email,
+      password_hash: hashed_password,
+      phone_number: doctorSignupDto.phone_number,
+      education: doctorSignupDto.education,
+      experience_years: doctorSignupDto.experience_years,
+      specialization: doctorSignupDto.specialization,
+      clinic_name: doctorSignupDto.clinic_name,
+      clinic_address: doctorSignupDto.clinic_address,
+      available_days: doctorSignupDto.available_days,
+      available_time_slots: doctorSignupDto.available_time_slots,
+    });
+
+    const savedDoctor = await this.doctorRepository.save(newDoctor);
+    savedDoctor.password_hash = '';
+
+    return savedDoctor;
   }
 
   async signin(doctorSigninDto: DoctorSigninDto) {
-    try {
-      const doctor = await this.doctorRepository.findOne({
-        where: { email: doctorSigninDto.email },
-      });
+    const doctor = await this.doctorRepository.findOne({
+      where: { email: doctorSigninDto.email },
+    });
 
-      if (!doctor) {
-        throw new UnauthorizedException('Invalid email or password');
-      }
-
-      const isPasswordValid = await this.verifyString(
-        doctorSigninDto.password,
-        doctor.password_hash,
-      );
-
-      if (!isPasswordValid) {
-        throw new UnauthorizedException('Invalid email or password');
-      }
-
-      const tokens = this.generateTokens(doctor);
-
-      doctor.hashed_refresh_token = await this.hashString(tokens.refreshToken);
-      await this.doctorRepository.save(doctor);
-
-      return tokens;
-    } catch {
-      throw new ServiceUnavailableException(
-        'Service is currently unavailable. Please try again later.',
-      );
+    if (!doctor) {
+      throw new UnauthorizedException('Invalid email or password');
     }
+
+    const isPasswordValid = await this.verifyString(
+      doctorSigninDto.password,
+      doctor.password_hash,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const tokens = this.generateTokens(doctor);
+
+    doctor.hashed_refresh_token = await this.hashString(tokens.refreshToken);
+    await this.doctorRepository.save(doctor);
+
+    return tokens;
   }
 
   private async hashString(string: string): Promise<string> {
@@ -114,7 +101,7 @@ export class AuthService {
 
     const accessToken = this.jwtService.sign(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: '1h',
+      expiresIn: '',
     });
 
     return { accessToken, refreshToken };
