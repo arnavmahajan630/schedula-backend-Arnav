c65c171dd6b23a18bad89b75c8edc42e72b84b61
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-06-19 22:48:58 +0530

    removed redundant refresh token changer

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -3,5 +3,3 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('access-token') {}
-@Injectable()
-export class RefreshTokenGuard extends AuthGuard('refresh-token') {}
