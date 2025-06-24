dc2333daa201a6a987bdcb1372987c15716d0b91
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-06-24 20:41:03 +0530

    resolved patientservice dependency in TypeormModule

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { PatientController } from './patient.controller';
 import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 import { PatientService } from './patient.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Patient } from './entities/patient.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Patient])],
   controllers: [PatientController],
   providers: [JwtStrategy, PatientService],
 })
