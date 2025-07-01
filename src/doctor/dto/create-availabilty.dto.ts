0761758405720ac7e797e82f3f07f17b24b09879
Abhishek Devanda <abdevanda2424@gmail.com>
2025-07-01 19:56:59 +0530

    fixed/dto and time-slot entity

--- src/doctor/dto/create-availabilty.dto.ts
+++ src/doctor/dto/create-availabilty.dto.ts
@@ -4,6 +4,9 @@ import {
   IsArray,
   ArrayNotEmpty,
   IsDate,
+  IsInt,
+  Min,
+  IsOptional,
 } from 'class-validator';
 import { Session, Weekday } from '../enums/availability.enums';
 import { Type } from 'class-transformer';
@@ -26,4 +29,13 @@ export class CreateDoctorAvailabilityDto {
   @ArrayNotEmpty()
   @IsEnum(Weekday, { each: true })
   weekdays: Weekday[];
+
+  @IsInt()
+  @Min(5) // Minimum 5 minutes
+  slot_duration: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  patients_per_slot?: number;
 }
